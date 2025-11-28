75d2dc952ccc87bd1f9cabfe9109103f67a2622a
poojam7040 <morepooja7040@gmail.com>
2025-11-28 12:16:28 +0530

    added

--- src/main/java/POM/Qspiders.java
+++ src/main/java/POM/Qspiders.java
@@ -5,6 +5,8 @@ public class Qspiders {
 	public static void main(String[] args) {
 		System.out.println("Pooja");
 		System.out.println("jjj");
+		System.out.println("jjj");
+		System.out.println("jjj");
 		System.out.println("working");
 	}
 }
